40bc414b57b8326b00cda70aa1b42aa902e141a4
Kovács Péter <peter.dunay.kovacs@gmail.com>
2025-11-21 11:52:18 +0100

    otel-grafana-stack / re-include service in log-processing
    This is a rather test-environment-specific consideration

--- otel-grafana-stack/run.sh
+++ otel-grafana-stack/run.sh
@@ -3,5 +3,6 @@
 # https://github.com/pdkovacs/forked-quickpizza
 grafana_stack_network=forked-quickpizza_default
 
-docker run --rm --network $grafana_stack_network --name bitkit bitkit/otel-grafana-stack
+# The conainer name conveniently includes a subnamespace-like "prefix"
+docker run --rm --network $grafana_stack_network --name bitkit_otel-grafana-stack bitkit/otel-grafana-stack
 
