5388d00eca7f82d51ec8561a5ddf642810d4ffd5
Kovács Péter <peter.dunay.kovacs@gmail.com>
2025-11-23 15:33:44 +0100

    otel-grafana-stack / quickpizza fork replaced with dedicated repo

--- otel-grafana-stack/deploy/docker/compose.sh
+++ otel-grafana-stack/deploy/docker/compose.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-grafana_stack_compose_home=${HOME}/github/pdkovacs/forked-quickpizza
-dcomp_project=forked-quickpizza
+grafana_stack_compose_home=${HOME}/github/pdkovacs/grafana-local-stack
+dcomp_project=grafana-local-stack
 
-infra_compose_file=$grafana_stack_compose_home/compose.grafana-local-stack.monolithic.yaml
+infra_compose_file=$grafana_stack_compose_home/compose.yaml
 compose_files="-f $infra_compose_file -f deploy/docker/service-with-lb.yaml"
 docker compose $compose_files down && docker compose $compose_files up -d
